fba2d6dfeee418792f9fc143b5bc09e0df47a1a5
lorrang10 <lorrangomesp30@gmail.com>
2024-08-23 18:02:45 -0300

    Criando Projeto

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["ovenda/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ovenda/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
